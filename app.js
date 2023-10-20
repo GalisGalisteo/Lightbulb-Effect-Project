b8b6b495abd91ee630782afb3f5b735d34b39ab1
GalisGalisteo <galis.travelling@gmail.com>
2023-10-20 10:34:54 +0200

    more solutions

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ const bulbHTML = document.querySelector("#bulb");
 //     }
 // })
 
+// bulbHTML.addEventListener('click', () => {
+//     if (bulbHTML.classList.contains('bulb-off')) {
+//         bulbHTML.classList.remove('bulb-off');
+//         bulbHTML.classList.add('bulb-on');
+//     } else {
+//         bulbHTML.classList.remove('bulb-on');
+//         bulbHTML.classList.add('bulb-off');
+//     }
+// })
+
+// let bulbON = false;
+
+// bulbHTML.addEventListener('click', () => {
+//     if (bulbON) {
+//         bulbON = false;
+//         bulbHTML.classList.remove('bulb-on');
+//     } else {
+//         bulbON = true;
+//         bulbHTML.classList.add('bulb-on');
+//     }
+// })
+
 bulbHTML.addEventListener('click', () => {
     bulbHTML.classList.toggle('bulb-on');
 })
