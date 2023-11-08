4a10db5621112688ca78b4adb4a95f3153a6a4be
GalisGalisteo <galis.travelling@gmail.com>
2023-11-08 13:40:05 +0100

    finishied using Vue

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
-// Buena suerte ! Espero que se te encianda la "bombilla"!
-
-const bulbHTML = document.querySelector("#bulb");
-
-// bulbHTML.addEventListener('click', () => {
-//     if (bulbHTML.className === 'bulb-off') {
-//         bulbHTML.className = 'bulb-on';
-//     } else {
-//         bulbHTML.className = 'bulb-off'
-//     }
-// })
-
-// bulbHTML.addEventListener('click', () => {
-//     if (bulbHTML.classList.contains('bulb-off')) {
-//         bulbHTML.classList.remove('bulb-off');
-//         bulbHTML.classList.add('bulb-on');
-//     } else {
-//         bulbHTML.classList.remove('bulb-on');
-//         bulbHTML.classList.add('bulb-off');
-//     }
-// })
-
-// let bulbON = false;
-
-// bulbHTML.addEventListener('click', () => {
-//     if (bulbON) {
-//         bulbON = false;
-//         bulbHTML.classList.remove('bulb-on');
-//     } else {
-//         bulbON = true;
-//         bulbHTML.classList.add('bulb-on');
-//     }
-// })
-
-bulbHTML.addEventListener('click', () => {
-    bulbHTML.classList.toggle('bulb-on');
-})
+import {
+    createApp,
+    ref,
+    computed,
+  } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+  
+  createApp({
+    setup() {
+      const isTrue = ref(false);
+      
+      return {
+        isTrue,
+      };
+    },
+  }).mount("#app");
+  
